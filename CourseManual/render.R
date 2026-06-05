c5a7bbee25f7fe7c498e12903d3ba99dd13d44dc
Laurence Frank <laurencefrank2019@gmail.com>
2026-06-05 13:26:30 +0200

    Upload course materials day 1 to 5

---
+++ CourseManual/render.R
@@ -0,0 +1,11 @@
+rmarkdown::render(
+  input = "CourseManual_RSummer2024.Rmd", 
+  output_file = "CourseManual_RSummer2024.html")
+
+
+rmarkdown::render(
+  input = "CourseManual_RSummer2024.Rmd", 
+  output_file = "CourseManual_download/CourseManual_RSummer2024.html")
+
+
+
